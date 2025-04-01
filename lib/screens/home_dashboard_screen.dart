d18bf4e603bb6f012afc45e279db764a5b9a599c
Shawn Mathew <Shawnm-MAD>
2025-03-31 20:29:40 -0400

    Add Work Log section with bar chart visualization capabilities for HomeDashboardScreen

--- lib/screens/home_dashboard_screen.dart
+++ lib/screens/home_dashboard_screen.dart
@@ -33,6 +33,35 @@ class HomeDashboardScreen extends StatelessWidget {
             'Work Log',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color:Colors.grey[200],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: BarChart(
+                BarChartData(
+                  barGroups: [
+                    BarChartGroupData(x: 0, barRods: [
+                      BarChartRodData(toY: 5, color: Colors.blue, width: 16)
+                    ]),
+                    BarChartGroupData(x: 1, barRods: [
+                      BarChartRodData(toY: 3, color: Colors.blue, width: 16)
+                    ]),
+                    BarChartGroupData(x: 2, barRods: [
+                      BarChartRodData(toY: 7, color: Colors.blue, width: 16)
+                    ]),
+                  ],
+                  titlesData: 
+                  FlTitlesData(showTitles:false),
+                 )
+              )
+              ),
+          )
+
         ],
       )
